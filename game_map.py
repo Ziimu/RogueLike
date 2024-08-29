ea9453d5abb5bbcab71e3e36de329e19845f027d
Siim Urbla <siimurbl@gmail.com>
2024-08-29 18:30:32 +0300

    fixed

--- game_map.py
+++ game_map.py
@@ -1,4 +1,4 @@
-import numpy as np
+import numpy as np  # type: ignore
 from tcod.console import Console
 
 import tile_types
@@ -10,7 +10,8 @@ class GameMap:
         self.tiles = np.full((width, height), fill_value=tile_types.wall, order="F")
 
     def in_bounds(self, x: int, y: int) -> bool:
-        return 0 <= x < self.width and 0 <= y < self.window_height
+        """Return True if x and y are inside of the bounds of this map."""
+        return 0 <= x < self.width and 0 <= y < self.height
 
     def render(self, console: Console) -> None:
         console.tiles_rgb[0:self.width, 0:self.height] = self.tiles["dark"]
