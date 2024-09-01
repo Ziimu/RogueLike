251bfa7ea2425f3df97398dac34d8de4469785df
Siim Urbla <siimurbl@gmail.com>
2024-09-01 12:15:00 +0300

    part6 muudatused

--- actions.py
+++ actions.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from typing import Optional, Tuple, TYPE_CHECKING
 
 if TYPE_CHECKING:
     from engine import Engine
@@ -8,39 +8,51 @@ if TYPE_CHECKING:
 
 
 class Action:
-    def perform(self, engine: Engine, entity: Entity) -> None:
-        """Perform this action with the objects needed to determine its scope.
-
-        `engine` is the scope this action is being performed in.
-
-        `entity` is the object performing the action.
-
-        This method must be overridden by Action subclasses.
-        """
+    
+    def __init__(self, entity: Entity) -> None:
+        super().__init__()
+        self.entity = entity
+        
+    @property
+    def engine(self) -> Engine:
+        #Return the engine this action belongs to
+        return self.entity.gamemap.engine
+    
+    def perform(self) -> None:
+        
+        #self.engine is the scope this action is being perforemd
+        #self.enitty is the object performint the action
+        
         raise NotImplementedError()
 
 
 class EscapeAction(Action):
-    def perform(self, engine: Engine, entity: Entity) -> None:
+    def perform(self) -> None:
         raise SystemExit()
 
 
 class ActionWithDirection(Action):
-    def __init__(self, dx: int, dy: int):
-        super().__init__()
+    def __init__(self, entity: Entity, dx: int, dy: int):
+        super().__init__(entity)
 
         self.dx = dx
         self.dy = dy
+        
+    @property
+    def dest_xy(self) -> Tuple[int, int]:
+        return self.entity.x + self.dx, self.entity.y + self.dy
+    
+    @property
+    def blocking_entity(self) -> Optional[Entity]:
+        return self.engine.game_map.get_blocking_entity_at_location(*self.dest_xy)
 
-    def perform(self, engine: Engine, entity: Entity) -> None:
+    def perform(self) -> None:
         raise NotImplementedError()
 
 
 class MeleeAction(ActionWithDirection):
-    def perform(self, engine: Engine, entity: Entity) -> None:
-        dest_x = entity.x + self.dx
-        dest_y = entity.y + self.dy
-        target = engine.game_map.get_blocking_entity_at_location(dest_x, dest_y)
+    def perform(self) -> None:
+        target = self.blocking_entity
         if not target:
             return  # No entity to attack.
 
@@ -48,27 +60,24 @@ class MeleeAction(ActionWithDirection):
 
 
 class MovementAction(ActionWithDirection):
-    def perform(self, engine: Engine, entity: Entity) -> None:
-        dest_x = entity.x + self.dx
-        dest_y = entity.y + self.dy
+    def perform(self) -> None:
+        dest_x, dest_y = self.dest_xy
+       
 
-        if not engine.game_map.in_bounds(dest_x, dest_y):
+        if not self.engine.game_map.in_bounds(dest_x, dest_y):
             return  # Destination is out of bounds.
-        if not engine.game_map.tiles["walkable"][dest_x, dest_y]:
+        if not self.engine.game_map.tiles["walkable"][dest_x, dest_y]:
             return  # Destination is blocked by a tile.
-        if engine.game_map.get_blocking_entity_at_location(dest_x, dest_y):
+        if self.engine.game_map.get_blocking_entity_at_location(dest_x, dest_y):
             return  # Destination is blocked by an entity.
 
-        entity.move(self.dx, self.dy)
+        self.entity.move(self.dx, self.dy)
 
 
 class BumpAction(ActionWithDirection):
-    def perform(self, engine: Engine, entity: Entity) -> None:
-        dest_x = entity.x + self.dx
-        dest_y = entity.y + self.dy
-
-        if engine.game_map.get_blocking_entity_at_location(dest_x, dest_y):
-            return MeleeAction(self.dx, self.dy).perform(engine, entity)
+    def perform(self) -> None:
+        if self.blocking_entity:
+            return MeleeAction(self.entity, self.dx, self.dy).perfom()
 
         else:
-            return MovementAction(self.dx, self.dy).perform(engine, entity)
+            return MovementAction(self.entity, self.dx, self.dy).perform()
--- entity.py
+++ entity.py
@@ -1,7 +1,8 @@
 from __future__ import annotations
 
 import copy
-from typing import Tuple, TypeVar, TYPE_CHECKING
+from typing import Optional, Tuple, TypeVar, TYPE_CHECKING
+
 
 if TYPE_CHECKING:
     from game_map import GameMap
@@ -12,15 +13,17 @@ class Entity:
     """
     A generic object to represent players, enemies, items, etc.
     """
+    gamemap: GameMap
+    
     def __init__(
        self,
+       gamemap: Optional[GameMap] = None,
        x: int = 0,
        y: int = 0,
        char: str = "?",
        color: Tuple[int, int, int] = (255, 255, 255),
        name: str = "<Unnamed>",
        blocks_movement: bool = False,
-      # render_order = int,
    ):
         self.x = x
         self.y = y
@@ -28,6 +31,10 @@ class Entity:
         self.color = color
         self.name = name
         self.blocks_movement = blocks_movement
+        if gamemap:
+           # If gamemap isn't provided now then it will be set later.
+           self.gamemap = gamemap
+           gamemap.entities.add(self)
        # self.render_order = render_order
 
     def spawn(self: T, gamemap: GameMap, x: int, y: int) -> T:
@@ -35,8 +42,19 @@ class Entity:
        clone = copy.deepcopy(self)
        clone.x = x
        clone.y = y
+       clone.gamemap = gamemap
        gamemap.entities.add(clone)
        return clone
+    
+    def place(self, x: int, y: int, gamemap: Optional[GameMap] = None) -> None:
+       """Place this entity at a new location.  Handles moving across GameMaps."""
+       self.x = x
+       self.y = y
+       if gamemap:
+           if hasattr(self, "gamemap"):  # Possibly uninitialized.
+               self.gamemap.entities.remove(self)
+           self.gamemap = gamemap
+           gamemap.entities.add(self) 
 
     def move(self, dx: int, dy: int) -> None:
         # Move the entity by a given amount
