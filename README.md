f74e4aa2dc63fed5b09fc21fdfae561d71bdf0f8
Siim Urbla <siimurbl@gmail.com>
2024-09-04 19:35:19 +0300

    part-7

--- README.md
+++ README.md
@@ -1,14 +1,2 @@
-(https://rogueliketutorials.com/tutorials/tcod/v2/part-7/)   ---POOLIK!!!
-
-Edit main.py like this: (siit alla ~50%)
-
-Diff Original
-        root_console = tcod.Console(screen_width, screen_height, order="F")
-        while True:
-+           root_console.clear()
-+           engine.event_handler.on_render(console=root_console)
-+           context.present(root_console)
--           engine.render(console=root_console, context=context)
-
-+           engine.event_handler.handle_events(context)
--           engine.event_handler.handle_events()
+(https://rogueliketutorials.com/tutorials/tcod/v2/part-7/)   ---Tehtud aga mouse over ei tööta.
+press V
--- engine.py
+++ engine.py
@@ -5,13 +5,12 @@ from typing import TYPE_CHECKING
 import tcod
 import tcod.constants
 
-from tcod.context import Context
 from tcod.console import Console
 from tcod.map import compute_fov
 
 from input_handlers import MainGameEventHandler
 from message_log import MessageLog
-from render_functions import render_bar
+from render_functions import render_bar, render_names_at_mouse_location
 
 if TYPE_CHECKING:
     from entity import Actor
@@ -25,6 +24,7 @@ class Engine:
    def __init__(self, player: Actor):
        self.event_handler: EventHandler = MainGameEventHandler(self)
        self.message_log = MessageLog()
+       self.mouse_location = (0, 0)
        self.player = player  
 
    def handle_enemy_turns(self) -> None:
@@ -45,7 +45,7 @@ class Engine:
         # If a tile is "visible" it should be added to "explored".
         self.game_map.explored |= self.game_map.visible
 
-   def render(self, console: Console, context: Context) -> None:
+   def render(self, console: Console) -> None:
         self.game_map.render(console)
         
         self.message_log.render(console=console, x=21, y=45, width=40, height=5) #kas see määrab selle asukoha?!
@@ -56,9 +56,9 @@ class Engine:
             maximum_value=self.player.fighter.max_hp,
             total_width=20,
         )
+        
+        render_names_at_mouse_location(console=console, x=21, y=44, engine=self)
 
     
 
-        context.present(console)
-
-        console.clear()
+      
